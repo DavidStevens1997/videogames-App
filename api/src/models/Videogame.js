f8e4fbe3f0d26909ce486b22dd2c3854b072a0f6
DavidStevens1997 <david.hr97110105243@gmail.com>
2022-12-04 12:34:04 -0500

    hacer las rutas y bd creada

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID, /*genera numeros y letras ramdom que no se va repetir*/
+      allowNull: false, 
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    relaseDate: {
+      type: DataTypes.STRING,
+    },
+    raiting: {
+      type: DataTypes.FLOAT,
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    createInDb: { /* todos los juegos que cree tienen esta propiedad */
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    }
   });
 };

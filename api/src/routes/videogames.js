39a7bf15f9aac0228bad15a97326d35fc7d4e892
DavidStevens1997 <david.hr97110105243@gmail.com>
2022-12-06 20:09:15 -0500

    trayengo videogames por get

--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,35 +1,35 @@
 const { Router } = require('express');
 const { Videogame , Genres }= require('../db');
 const {Op} = require('sequelize');
-const axios  = require('axios');
+const axios  = require('axios').default;
 const router = Router();
 const { API_KEY } = process.env;
 
 router.get('/' , async(req,res,next) => {
     try {
-        let resultsToReturn = [];
-        let rawgApi = (`https://api.rawg.io/api/games?page=${i}&key=${API_KEY}`);
-  
-        for (let i = 0; i < 3; i++) {
-          let videogames = (await axios.get(rawgApi)).data;
-          let gameInfo = videogames.results.map((videogame) => {
-            return {
-              background_image: videogame.background_image,
-              name: videogame.name,
-              genres: videogame.genres.map((genre) => genre.name),
-              id: videogame.id,
-              rating: videogame.rating,
-            };
-          });
-          rawgApi = videogames.next;
-          resultsToReturn = resultsToReturn.concat(gameInfo);
+      let resultsToReturn = [];
+      let rawgApi = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page_size=5`, {headers:{"accept-encoding":'*'}});
+      let dataApi = rawgApi.data;
+      let resultsApi = dataApi.results; 
+      let gameInfo = resultsApi.map((videogame) => {
+        return {
+            background_image: videogame.background_image,
+            name: videogame.name,
+            genres: videogame.genres.map((genre) => genre.name),
+            id: videogame.id,
+            rating: videogame.rating,
         }
-        console.log(allGames);
-      return res.status(200).send(allGames);
-         
+      });
+      resultsToReturn = resultsToReturn.concat(gameInfo);
+      
+      /* let rawgApi = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page_size=1`, {headers:{"accept-encoding":'*'}});
+      let oneGame = rawgApi.data;
+      let otherGame = oneGame.results
+      console.log(otherGame); */ 
+      return res.status(200).json(resultsToReturn);
 
     } catch (error) {
-        //console.log(error)
+        console.log(error)
         res.status(400).send('Something went wrong!');
     }
 });
